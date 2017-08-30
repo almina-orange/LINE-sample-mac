d9b3bcbcf2cb203ac1ec87d8c56bba02a6394207
almina-mac <alm.0554.mina@gmail.com>
2017-08-30 19:13:45 +0900

    edit index: add imgmes, img

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // all read library installed by composer
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 // instancing CurlHTTPClient using access token
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
@@ -9,17 +9,21 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 // get signature assigned LINE Messaging API to request
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$signature = $_SERVER['HTTP_'.\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 // check correctness of signature. if correct, parse and storage into array
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 // process rooply each events in array
 foreach ($events as $event) {
-  // reply message and next event
+  // reply message and proceed next event
   replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+
+  // reply image and proceed next event
+  replyImageMessage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'/img/original.jpg', 'https://'.$_SERVER['HTTP_HOST'].'/img/preview.jpg');
 }
 
+/*----- function -----*/
 // text reply
 function replyTextMessage($bot, $replyToken, $text) {
   // reply and get response
@@ -28,7 +32,19 @@ function replyTextMessage($bot, $replyToken, $text) {
   // if response is storange
   if (!$response->isSucceeded()) {
     // output error
-    error_log('Failed! ' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// image reply
+function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl) {
+  // reply and get response
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+
+  // if response is storange
+  if (!$response->isSucceeded()) {
+    // output error
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
   }
 }
 ?>
