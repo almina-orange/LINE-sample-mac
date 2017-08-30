1176d569d89e0db51b8668d818d614b99d731f60
almina-mac <alm.0554.mina@gmail.com>
2017-08-30 16:15:59 +0900

    heroku: add composer, vendor, index

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-echo "Hello heroku world!";
-echo "We waiting for you!";
+// all read library installed by composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// get and preview values sent in POST method
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 ?>
