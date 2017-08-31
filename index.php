17f3cf99a559e953174e7bb1d1b14b741556fda8
almina-mac <alm.0554.mina@gmail.com>
2017-08-31 17:44:25 +0900

    edit index: add location, sticker, video, audio, multiMessage

--- index.php
+++ index.php
@@ -16,11 +16,30 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 // process rooply each events in array
 foreach ($events as $event) {
+  // collect multi messages
+  $replyText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('TextMessage');
+  $replyImg = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://'.$_SERVER['HTTP_HOST'].'/img/original.jpg', 'https://'.$_SERVER['HTTP_HOST'].'/img/preview.jpg');
+  $replyLoc = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
+  $replySticker = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 1);
+  replyMultiMessage($bot, $event->getReplyToken(), $replyText, $replyImg, $replyLoc, $replySticker);
+
   // reply message and proceed next event
   // replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
 
   // reply image and proceed next event
-  replyImageMessage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'/img/original.jpg', 'https://'.$_SERVER['HTTP_HOST'].'/img/preview.jpg');
+  // replyImageMessage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'/img/original.jpg', 'https://'.$_SERVER['HTTP_HOST'].'/img/preview.jpg');
+
+  // reply location and proceed next event
+  // replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
+
+  // reply sticker and proceed newx event
+  // replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
+
+  // reply video and proceed next event
+  // replyVideoMessage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'/video/sample.mp4', 'https://'.$_SERVER['HTTP_HOST'].'/video/sample_preview.jpg');
+
+  // reply audio and proceed next event
+  // replyAudioMessage($bot, $event->getReplyToken(), 'https://'.$_SERVER['HTTP_HOST'].'/audio/sample.m4a', 6000);
 }
 
 /*----- function -----*/
@@ -44,6 +63,64 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUr
   // if response is storange
   if (!$response->isSucceeded()) {
     // output error
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// location reply
+function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
+  // location, longitude, latitude
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+
+  if (!$response->isSucceeded()) {
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// sticker reply
+function replyStickerMessage($bot, $replyToken, $packageId, $stickerId) {
+  // sticker
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId, $stickerId));
+
+  if (!$response->isSucceeded()) {
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// video reply
+function replyVideoMessage($bot, $replyToken, $originalContentUrl, $previewImageUrl) {
+  // video
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\VideoMessageBuilder($originalContentUrl, $previewImageUrl));
+
+  if (!$response->isSucceeded()) {
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// audio reply
+function replyAudioMessage($bot, $replyToken, $originalContentUrl, $audioLength) {
+  // audio
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\AudioMessageBuilder($originalContentUrl, $audioLength));
+
+  if (!$response->isSucceeded()) {
+    error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
+  }
+}
+
+// multi message reply
+function replyMultiMessage($bot, $replyToken, ...$msgs) {
+  // instancing MultiMessageBuilder
+  $bulder = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
+
+  // add all messages in builder
+  foreach($msgs as $value) {
+    $bulder->add($value);
+  }
+
+  // multi message
+  $response = $bot->replyMessage($replyToken, $builder);
+
+  if (!$response->isSucceeded()) {
     error_log('Failed! '.$response->getHTTPStatus.' '.$response->getRawBody());
   }
 }
